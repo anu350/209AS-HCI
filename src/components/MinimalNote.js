d8615aa5c9ec47124e43c3387e59e2b623561720
oykudenizbozkurt <oykudenizbozkurt@gmail.com>
2022-01-29 18:04:42 -0800

    Add files via upload

--- src/components/MinimalNote.js
+++ src/components/MinimalNote.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 import "react-quill/dist/quill.bubble.css";
+import { createClient } from '@supabase/supabase-js';
 
 class MinimalTextEditor extends Component {
   constructor(props) {
@@ -22,13 +23,47 @@ class MinimalTextEditor extends Component {
     };
 
     this.rteChange = this.rteChange.bind(this);
+
+    this.currentText = "";
+
+    this.supabaseUrl = process.env.REACT_APP_SUPABASE_URL
+    this.supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY
+    this.supabase = createClient(this.supabaseUrl, this.supabaseAnonKey)
   }
 
   rteChange = (content, delta, source, editor) => {
-    console.log(editor.getHTML()); // rich text
-    console.log(editor.getText()); // plain text
-    console.log(editor.getLength()); // number of characters
+    //console.log(editor.getHTML()); // rich text
+    //console.log(editor.getText()); // plain text
+    //console.log(editor.getLength()); // number of characters
+    this.currentText = editor.getText();
   };
+
+  saveText = () => {
+    this.add_note(this.currentText);
+    alert('Saved!');
+  }
+
+  add_note = async (input) => {
+    await this.supabase
+      .from('notes')
+      .insert([
+        { note: input}
+      ])
+  }
+
+  delete_note = async () => {
+    await this.supabase
+    .from('notes')
+    .delete()
+    .match({ id: 1})
+  }
+
+  fetch_note = async () => {
+    await this.supabase
+      .from('notes')
+      .select('note')
+      .then(console.log)
+  }
 
   render() {
     return (
@@ -40,6 +75,8 @@ class MinimalTextEditor extends Component {
           onChange={this.rteChange}
           value={this.state.comments || ""}
         />
+        <button onClick={this.saveText}>Save</button>
+
       </div>
     );
   }
