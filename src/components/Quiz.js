85439b972a5dadea89f7e846e51484c07d9fa523
anu350 <36208996+anu350@users.noreply.github.com>
2022-02-21 15:51:15 -0800

    Add files via upload

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { supabase } from "../lib/supabaseClient";
 import Question from "./Question";
 import "../../node_modules/font-awesome/css/font-awesome.min.css"; 
+import QuizContainer from "./QuizContainer";
 
 export default function Quiz(props) {
     const [myquestions, setQuestions] = useState([]);
@@ -9,133 +10,131 @@ export default function Quiz(props) {
     const [loading, setLoading] = useState(false);
     const [quizMode, setQuizMode] = useState(false);
 
+     
+    const onClear = async () => {
+      if (empty) {
+        return;
+      }
+      setQuestions([]);
+      let { data, error } = await supabase
+        .from("questions")
+        .delete()
+        .match({ related_note: props.noteId });
+      if (error) console.log("error deleting questions", error);
+      setEmpty(true);
+      return;
+    };
+
+  useEffect(() => {
+      console.log("props.id:", props.noteId);
+      fetchQuestions(props.noteId);
+    }, []);
+  
+  useEffect(() => {
+     if (myquestions.length) {
+       setEmpty(false);
+     } else {
+       setEmpty(true);
+     }
+   }, [myquestions]);
+
+   const fetchQuestions = async (my_id) => {
+      const { data: qs, error } = await supabase
+        .from("questions")
+        .select("question")
+        .eq("related_note", my_id);
+      if (error) console.log("error fetching questions", error);
+      else {
+        if (qs.length) {
+          console.log("fetched questions:", qs);
+          // qs.map((aq) => console.log(aq.question));
+          qs.map((aq) =>
+            setQuestions((myquestions) => [...myquestions, aq.question])
+          );
+        }
+      }
+      console.log("myquestions: ", myquestions);
+    };
+
+  //SAVE QUESTIONS TO SUPABASE
+  const saveQuestions = async () => {
+      const creation_time = new Date().toISOString();
+      const { data, error } = await supabase.from("questions").insert(
+        myquestions.map((q, idx) => ({
+          created_at: creation_time,
+          related_note: props.noteId,
+          question: q,
+          answer: "",
+          index: idx,
+        }))
+      );
+  
+      // let data2post =
+      console.log(
+        myquestions.map((q, idx) => ({
+          created_at: creation_time,
+          related_note: props.noteId,
+          question: q,
+          answer: "",
+          index: idx,
+        }))
+      );
+    };  
+
+  //GENERATE QUESTION
+  const onGenerate = () => {
+      setLoading(true);
+      fetch("http://localhost:5000/notes/", {
+        method: "POST",
+        // headers: { CORS: "Access-Control-Allow-Origin" },
+        headers: {
+          // CORS: "Access-Control-Allow-Origin",
+          "content-type": "application/json",
+          // accepts: "application/json",
+        },
+        body: JSON.stringify({
+          title: "test title",
+          raw_json: props.fullnote.raw_json,
+        }),
+      })
+        .then((response) => {
+          return response.json();
+        })
+        .then((json) => {
+          console.log("full resposnse:", json);
+          if (json[2] === 201) {
+            console.log(Array.isArray(json[1]));
+            json[1].map(
+              (x) => setQuestions((myquestions) => [...myquestions, x.question]) // error ---------------
+            );
+          }
+          setLoading(false);
+        });
+      // ------------------
+      // NOTE:
+      // can use this function as the generate button
+      // make an async call to python
+      // python returns an array
+      // array holds questions
+      // if non empty, post to supabase DB
+    };
+  
+
     const onclickQuiz = () => {
         //generate questions and save to supabase
         console.log("Initiating Quiz Mode");
-        onGenerate();
-        saveQuestions();
+        //onGenerate();
+        //saveQuestions();
         setQuizMode(!quizMode);
       };
-    
+   
     const closeQuiz = () => {
-       // console.log("clicked question button");
-       setQuizMode(!quizMode);
-       onClear();
-     };
-
-     const nextQuestion = () => {
-       //set the next question from json[1] array and 4 answer choices
-
-     };
-
-     const onClear = async () => {
-        if (empty) {
-          return;
-        }
-        setQuestions([]);
-        let { data, error } = await supabase
-          .from("questions")
-          .delete()
-          .match({ related_note: props.noteId });
-        if (error) console.log("error deleting questions", error);
-        setEmpty(true);
-        return;
+      //generate questions and save to supabase
+      console.log("Quitting Quiz Mode");
+      setQuizMode(!quizMode);
       };
 
-    useEffect(() => {
-        console.log("props.id:", props.noteId);
-        fetchQuestions(props.noteId);
-      }, []);
-    
-    useEffect(() => {
-       if (myquestions.length) {
-         setEmpty(false);
-       } else {
-         setEmpty(true);
-       }
-     }, [myquestions]);
-
-     const fetchQuestions = async (my_id) => {
-        const { data: qs, error } = await supabase
-          .from("questions")
-          .select("question")
-          .eq("related_note", my_id);
-        if (error) console.log("error fetching questions", error);
-        else {
-          if (qs.length) {
-            console.log("fetched questions:", qs);
-            // qs.map((aq) => console.log(aq.question));
-            qs.map((aq) =>
-              setQuestions((myquestions) => [...myquestions, aq.question])
-            );
-          }
-        }
-        console.log("myquestions: ", myquestions);
-      };
-
-    //SAVE QUESTIONS TO SUPABASE
-    const saveQuestions = async () => {
-        const creation_time = new Date().toISOString();
-        const { data, error } = await supabase.from("questions").insert(
-          myquestions.map((q, idx) => ({
-            created_at: creation_time,
-            related_note: props.noteId,
-            question: q,
-            answer: "",
-            index: idx,
-          }))
-        );
-    
-        // let data2post =
-        console.log(
-          myquestions.map((q, idx) => ({
-            created_at: creation_time,
-            related_note: props.noteId,
-            question: q,
-            answer: "",
-            index: idx,
-          }))
-        );
-      };  
-
-    //GENERATE QUESTION
-    const onGenerate = () => {
-        setLoading(true);
-        fetch("http://localhost:5000/notes/", {
-          method: "POST",
-          // headers: { CORS: "Access-Control-Allow-Origin" },
-          headers: {
-            // CORS: "Access-Control-Allow-Origin",
-            "content-type": "application/json",
-            // accepts: "application/json",
-          },
-          body: JSON.stringify({
-            title: "test title",
-            raw_json: props.fullnote.raw_json,
-          }),
-        })
-          .then((response) => {
-            return response.json();
-          })
-          .then((json) => {
-            console.log("full resposnse:", json);
-            if (json[2] === 201) {
-              console.log(Array.isArray(json[1]));
-              json[1].map(
-                (x) => setQuestions((myquestions) => [...myquestions, x.question]) // error ---------------
-              );
-            }
-            setLoading(false);
-          });
-        // ------------------
-        // NOTE:
-        // can use this function as the generate button
-        // make an async call to python
-        // python returns an array
-        // array holds questions
-        // if non empty, post to supabase DB
-      };
-    
+     
   const buttonHover = (event) => {
     event.target.style.backgroundColor = "#abc6fd";
     event.target.style.transitionDuration = "0.2s";
@@ -149,45 +148,25 @@ export default function Quiz(props) {
 
   return (
     <div style = {styles.qcontainer}>
-        {/* if button toggled, display form*/}
+        {/* if button toggled, display quiz*/}
         {quizMode ? (
-        <div style = {styles.quizContainer}>
-            <button className="fa fa-close" style={styles.closeButton} onClick={closeQuiz} onMouseEnter={buttonHover} onMouseLeave={stopButtonHover}>
-            </button>
-            <button className="fa fa-arrow-right" style={styles.nextButton} onClick={nextQuestion} onMouseEnter={buttonHover} onMouseLeave={stopButtonHover}>
-            </button>
-
-            {/* DISPLAY QUESTIONS AND MULTIPLE CHOICE HERE ONE AT A TIME INSTEAD OF MAPPING AT ONCE*/}
-        {   
-            {
-                false: (
-                <div>
-                    {myquestions.map((myQ, index) => (
-                    <div
-                        style={styles.questions}
-                        key={index}
-                        // noteId={props.noteId}
-                    >
-                        <Question question={myQ} />
-                    </div>
-                    ))}
-                </div>
-                ),
-                true: <div>{loading ? <p> Loading</p> : <p>No questions</p>} </div>,
-            }[empty]
-        }
-
-        </div> 
+          <div> 
+             <button style={styles.quizButton} onClick={closeQuiz} onMouseEnter={buttonHover} onMouseLeave={stopButtonHover}>
+               Close Quiz
+            </button> 
+            <div><br></br></div>
+            <QuizContainer
+              //PASS IN QUESTIONS/ANSWERS TO QUIZ CONTAINER IN CORRECT FORMAT
+            />
+          </div>
         ):(
-            <div >
+            <div>
                 <button style={styles.quizButton} onClick={onclickQuiz} onMouseEnter={buttonHover} onMouseLeave={stopButtonHover}>
                     Take Quiz
                 </button>
-                <div style = {styles.resultsContainer}>
+                {/* <div style = {styles.resultsContainer}>
                     RESULTS:
-                    {/* DISPLAY QUIZ RESULTS HERE IN GRAY CONTAINER */}
-
-                </div>
+                </div> */}
             </div>
          )}
     </div>
@@ -237,18 +216,18 @@ const styles = {
     left: "40%", 
   },
   closeButton: {
-    height: "50px",
+    height: "40px",
     width: "10%",
     fontSize: "15px",
-    padding: "0px",
+    padding: "10px",
     position: "relative",
-    left: "85%",
-    top: "5%",
-    borderRadius: "8px",
+    left: "75%",
+    borderRadius: "20px",
     backgroundColor: "white", 
     color: "black", 
     border: "1px solid #abc6fd",
     cursor: "pointer", 
+    bottomMargin:"10px",
   },
 
   nextButton: {
